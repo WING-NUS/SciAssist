53675c46d00d93a4237fafbdcb40c604d9399809
kirinzhu <linxiao.zhu@outlook.com>
2023-04-01 01:01:26 +0800

    dataset extraction v1.0

--- src/SciAssist/pipelines/__init__.py
+++ src/SciAssist/pipelines/__init__.py
@@ -8,9 +8,11 @@ from SciAssist import BASE_CACHE_DIR
 from SciAssist.models.components.bart_summarization import BartForSummarization
 from SciAssist.models.components.bert_token_classifier import BertForTokenClassifier
 from SciAssist.models.components.flant5_summarization import FlanT5ForSummarization
+from SciAssist.models.components.bert_dataset_extraction import BertForDatasetExtraction
 from SciAssist.utils.data_utils import (
     DataUtilsForTokenClassification,
     DataUtilsForSeq2Seq,
+    DataUtilsForDatasetExtraction
 )
 
 # Provided models for each task
@@ -46,6 +48,14 @@ TASKS = {
         }
     },
 
+    "dataset-extraction": {
+        "default": {
+            "model": BertForDatasetExtraction,
+            "model_dict_url": "https://huggingface.co/spaces/wing-nus/SciAssist/resolve/main/dataset-extraction.pt",
+            "data_utils": DataUtilsForDatasetExtraction,
+        },
+    },
+
     # "controlled-summarization": {
     #     "default": {
     #         "model": FrostForSummarization,
@@ -55,6 +65,7 @@ TASKS = {
     # }
 
 }
+
 
 def load_model(config: Dict, cache_dir=BASE_CACHE_DIR, device="gpu"):
     '''
@@ -79,6 +90,11 @@ def load_model(config: Dict, cache_dir=BASE_CACHE_DIR, device="gpu"):
     if config["model_dict_url"]!=None:
         state_dict = torch.hub.load_state_dict_from_url(config["model_dict_url"], model_dir=cache_dir, map_location=map_location)
         model.load_state_dict(state_dict)
+    else:
+        # You can also choose to load your local trained model:
+        # model.load_state_dict(torch.load("/home/linxiao/SciAssist-scibert-0223/src/models/scibert_ner/2023-03-25_02-27-17/scibert_dataset_extraction.pt"))
+        pass
+
     model.eval()
 
     return model
