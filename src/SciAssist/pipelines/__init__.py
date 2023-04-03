98423d47dc4c3ccbe0ec5e756af7726f9a638bd8
dyxohjl666 <dingyixi@hotmail.com>
2023-04-03 10:18:02 +0800

    Add pipeline configs
    Signed-off-by: dyxohjl666 <dingyixi@hotmail.com>

--- src/SciAssist/pipelines/__init__.py
+++ src/SciAssist/pipelines/__init__.py
@@ -5,12 +5,11 @@ from typing import Dict
 import torch
 
 from SciAssist import BASE_CACHE_DIR
-from SciAssist.models.components.bart_summarization import BartForSummarization
 from SciAssist.models.components.bert_token_classifier import BertForTokenClassifier
 from SciAssist.models.components.flant5_summarization import FlanT5ForSummarization
 from SciAssist.utils.data_utils import (
     DataUtilsForTokenClassification,
-    DataUtilsForSeq2Seq,
+    DataUtilsForSeq2Seq, DataUtilsForFlanT5, DataUtilsForT5,
 )
 
 # Provided models for each task
@@ -30,29 +29,35 @@ TASKS = {
 
     "single-doc-summarization": {
         "bart-cnn-on-mup": {
-            "model": BartForSummarization,
-            "model_dict_url": "https://huggingface.co/spaces/wing-nus/SciAssist/resolve/main/bart-large-cnn-e5.pt",
+            "model": FlanT5ForSummarization,
+            "model_dict_url": "https://huggingface.co/spaces/dyxohjl666/Controlled-summarization/resolve/main/flant5-mup-scisumm.pt",
             "data_utils": DataUtilsForSeq2Seq,
         },
         "default": {
-            "model": BartForSummarization,
-            "model_dict_url": "https://huggingface.co/spaces/wing-nus/SciAssist/resolve/main/bart-large-cnn-e5.pt",
+            "model": FlanT5ForSummarization,
+            "model_dict_url": "https://huggingface.co/spaces/dyxohjl666/Controlled-summarization/resolve/main/flant5-mup-scisumm.pt",
             "data_utils": DataUtilsForSeq2Seq,
+        },
+        "t5": {
+            "model": FlanT5ForSummarization,
+            "model_dict_url": None,
+            "data_utils": DataUtilsForT5,
         },
         "flan-t5": {
             "model": FlanT5ForSummarization,
             "model_dict_url": None,
-            "data_utils": DataUtilsForSeq2Seq,
+            "data_utils": DataUtilsForFlanT5,
         }
     },
 
-    # "controlled-summarization": {
-    #     "default": {
-    #         "model": FrostForSummarization,
-    #         "model_dict_url": None,
-    #         "data_utils": DataUtilsForFrost,
-    #     }
-    # }
+    "controlled-summarization": {
+        "default": {
+            "model": FlanT5ForSummarization,
+            "model_dict_url": "https://huggingface.co/spaces/dyxohjl666/Controlled-summarization/resolve/main/flant5-mup-scisumm.pt",
+            # "model_dict_url": None,
+            "data_utils": DataUtilsForFlanT5,
+        }
+    }
 
 }
 
